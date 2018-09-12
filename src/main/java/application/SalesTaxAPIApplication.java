2e94c22f49276ef42aa28e9ebacde902dae67000
Ernest Holloway <ernest.holloway@embersoftwarellc.com>
2018-09-12 00:04:35 -0500

    Got the Spring dependencies to properly compile within Gradle.

--- src/main/java/application/SalesTaxAPIApplication.java
+++ src/main/java/application/SalesTaxAPIApplication.java
@@ -1,10 +1,23 @@
-package application;//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+package application;
 
-//@SpringBootApplication
+import facades.SalesTaxSeederFacade;
+import modules.AppComponent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.inject.Inject;
+
+@SpringBootApplication
 public class SalesTaxAPIApplication {
+    private static AppComponent appComponent;
+
+    @Inject
+    SalesTaxSeederFacade salesTaxSeederFacade;
 
     public static void main(String[] args) {
-//        SpringApplication.run(application.SalesTaxAPIApplication.class, args);
+        //Sets up the Dagger dependency injection graph for the entire application.
+//        appComponent = DaggerAppComponent.builder().application(this).build();
+
+        SpringApplication.run(application.SalesTaxAPIApplication.class, args);
     }
 }
